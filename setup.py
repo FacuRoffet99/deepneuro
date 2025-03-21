06b5627018dc188d53e3a6bd319472d25ba073d1
FacuRoffet99 <facundo.roffet@cs.uns.edu.ar>
2025-03-21 12:54:49 +0100

    conversion to package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deepneuro',
+    version='0.1.0',
+    author='Facundo Roffet',
+    author_email='facundo.roffet@cs.uns.edu.ar',
+    url='https://github.com/FacuRoffet99/deepneuro',
+    packages=find_packages(),
+    include_package_data=True,
+)
